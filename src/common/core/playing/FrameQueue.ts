0fdd9555b4cd2b67ea1908dc69ff251fd7ff1ef5
jet2jet <34238471+jet2jet@users.noreply.github.com>
2021-09-03 18:49:17 +0900

    Reduce Float32Array instance creation

--- src/common/core/playing/FrameQueue.ts
+++ src/common/core/playing/FrameQueue.ts
@@ -1,15 +1,12 @@
 export default class FrameQueue {
 	private frames: Array<[ArrayBuffer, ArrayBuffer] | string> = [];
-	private curFrames: [Float32Array, Float32Array] | string | undefined;
+	private curFrames: [ArrayBuffer, ArrayBuffer] | string | undefined;
 	private offset: number = 0;
 	private queuedFrames = 0;
 
 	public pushFrames(rawFrames: [ArrayBuffer, ArrayBuffer]) {
 		if (typeof this.curFrames === 'undefined') {
-			this.curFrames = [
-				new Float32Array(rawFrames[0]),
-				new Float32Array(rawFrames[1]),
-			];
+			this.curFrames = rawFrames;
 		} else {
 			this.frames.push(rawFrames);
 		}
@@ -51,15 +48,21 @@ export default class FrameQueue {
 			if (typeof cf === 'string') {
 				cbMarker(cf, framesCopied);
 			} else {
-				const bufferFrames = cf[0].length;
+				const bufferFrames = cf[0].byteLength / 4;
 				const offset = this.offset;
 				let copyFrames = dest[0].length - framesCopied;
 				if (offset + copyFrames > bufferFrames) {
 					copyFrames = bufferFrames - offset;
 				}
 				const lastFrame = offset + copyFrames;
-				dest[0].set(cf[0].subarray(offset, lastFrame), framesCopied);
-				dest[1].set(cf[1].subarray(offset, lastFrame), framesCopied);
+				dest[0].set(
+					new Float32Array(cf[0], offset * 4, copyFrames),
+					framesCopied
+				);
+				dest[1].set(
+					new Float32Array(cf[1], offset * 4, copyFrames),
+					framesCopied
+				);
 				framesCopied += copyFrames;
 				if (lastFrame < bufferFrames) {
 					this.offset = lastFrame;
@@ -84,9 +87,6 @@ export default class FrameQueue {
 		if (typeof newF === 'string') {
 			return (this.curFrames = newF);
 		}
-		return (this.curFrames = [
-			new Float32Array(newF[0]),
-			new Float32Array(newF[1]),
-		]);
+		return (this.curFrames = newF);
 	}
 }
