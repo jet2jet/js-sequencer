611fcd7160a9bab2891d1c4e4b0f93e5ecd5ffa2
jet2jet <34238471+jet2jet@users.noreply.github.com>
2024-05-01 19:53:26 +0900

    Fix BeatsCalculator miss

--- src/main/core/BeatsCalculator.ts
+++ src/main/core/BeatsCalculator.ts
@@ -5,21 +5,22 @@ export default class BeatsCalculator {
 	public posDenominator: number = 4;
 	public beatsNumerator: number = 4;
 	public beatsDenominator: number = 4;
-	public beatsDenPerGcd: number = 1; // beatsDenominator / gcd
+	public posDenPerGcd: number = 1; // posDenominator / gcd
 
-	public incrementPosition(delta: number) {
-		this.posNumerator += delta * this.beatsDenPerGcd;
+	public incrementPosition(delta: number): void {
+		this.posNumerator += delta * this.posDenPerGcd;
 	}
 	public changeTimeSignature(
 		beatsNumerator: number,
 		beatsDenominator: number
-	) {
+	): void {
 		const g = gcd(this.posDenominator, beatsDenominator);
 		const q = beatsDenominator / g;
-		// this.beatsDenPerGcd = this.posDenominator / g;
-		this.beatsDenPerGcd = q;
+		// this.posDenominator = this.posDenominator / g;
 		this.posNumerator *= q;
 		this.posDenominator *= q;
+		this.posDenPerGcd =
+			this.posDenominator / gcd(this.posDenominator, beatsDenominator);
 		this.beatsNumerator = beatsNumerator;
 		this.beatsDenominator = beatsDenominator;
 	}
